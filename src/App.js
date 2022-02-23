bd7118624d793a4255a4ecb9bcef5f75cca9597c
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-23 13:34:13 +0100

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -5,30 +5,16 @@ import { v4 as randomId } from 'uuid';
 import FoodBox from './components/FoodBox';
 
 function App() {
-const [allFoods, setFoods] = useState(foods);
-
-
+  const [allFoods, setFoods] = useState(foods);
 
   return (
     <div className={'App' + allFoods}>
       <h1>Food List</h1>
-      <FoodBox
-        food={{
-          name: 'Orange',
-          calories: 85,
-          image: 'https://i.imgur.com/abKGOcv.jpg',
-          servings: 1,
-        }}
-      />
-      {/* {allFoods.map(function (foods) {
-       return (
-         <div key={randomId()}>
-           <p> {foods.name} </p>
-           <img src={foods.image} width={200} alt="food" />
-         </div>
-       );
-      }
-      )} */}
+      <div>
+        {allFoods.map((item) => {
+          return <FoodBox key={item.name} food={item} />;
+        })}
+      </div>
     </div>
   );
 }
